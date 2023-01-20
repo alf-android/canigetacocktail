34c07acefb7bbb845565f5ebbd505a28fb95d67d
Aitor Lagunas <alagunas@shotl.com>
2023-01-20 11:56:50 +0100

    refactor: separate viewmodel directory from ui directory

--- app/src/main/java/com/alagunas/canigetacocktail/ui/activities/MainActivity.kt
+++ app/src/main/java/com/alagunas/canigetacocktail/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.alagunas.canigetacocktail
+package com.alagunas.canigetacocktail.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alagunas.canigetacocktail.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
